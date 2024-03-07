dae455cfcf03e0b5733989496f5eb2f56121c533
Summerive <transunitecher@gmail.com>
2024-03-08 01:27:32 +0900

    new startup messages

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,7 @@ import { natsWrapper } from "./nats-wrapper";
 import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 
 const start = async () => {
+  console.log("Starting...");
   if (!process.env.JWT_KEY) {
     if (!process.env.NATS_CLUSTER_ID) {
       throw new Error("MONGO_URI must be defined");
